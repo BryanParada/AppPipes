be5f3e16438ff189b7fe866a65031cf8fad6671b
Bryan Parada <bryan.parada.cea@gmail.com>
2022-10-15 14:33:49 -0300

    V142 - PrimeFlex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+
 import { PrimeNGModule } from './prime-ng/prime-ng.module';
-import { SharedModule } from './shared/shared.module';
+
 import { AppRouterModule } from './app-router.module';
+import { VentasModule } from './ventas/ventas.module';
+import { SharedModule } from './shared/shared.module';
 
 
 
@@ -16,7 +19,8 @@ import { AppRouterModule } from './app-router.module';
     BrowserModule, 
     PrimeNGModule,
     SharedModule,
-    AppRouterModule
+    AppRouterModule,
+    VentasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
