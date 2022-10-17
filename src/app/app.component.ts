994ccaea2bf8cb3998b89cf4f02ed677d41e7714
Bryan Parada <bryan.parada.cea@gmail.com>
2022-10-17 11:34:37 -0300

    V149 - FieldSet - BrowserAnimations (Bubble)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pipesApp';
+export class AppComponent implements OnInit {
+ 
+  constructor(private primengConfig: PrimeNGConfig) {}
 
-  nombre: string = "bryAn parada";
-  valor: number = 1000;
-  obj = {
-    nombre: 'bryan'
-  }
-
-  mostrarNombre(){
-    console.log(this.nombre);
-    console.log(this.valor);
-    console.log(this.obj);
-    
-    
+  ngOnInit() {
+    this.primengConfig.ripple = true;
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
@@ -8,6 +9,8 @@ import { PrimeNGModule } from './prime-ng/prime-ng.module';
 import { AppRouterModule } from './app-router.module';
 import { VentasModule } from './ventas/ventas.module';
 import { SharedModule } from './shared/shared.module';
+
+
 
 // Cambiar el locale de la app
 import localeEs from "@angular/common/locales/es-CL";
@@ -23,10 +26,11 @@ registerLocaleData(localeFr);
   ],
   imports: [
     BrowserModule, 
+    BrowserAnimationsModule,
     PrimeNGModule,
     SharedModule,
     AppRouterModule,
-    VentasModule
+    VentasModule  
   ],
   providers: [
     { provide: LOCALE_ID, useValue: 'es-CL'}
