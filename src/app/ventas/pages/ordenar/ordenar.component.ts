d61a9da6a1ededaf05f6760af8aa8c0a1ffecf97
Bryan Parada <bryan.parada.cea@gmail.com>
2022-10-17 21:25:09 -0300

    V163-164 - PrimeTable & PrimeToolbar

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MayusculasPipe } from '../../pipes/mayusculas.pipe';
+import { Heroe, Color } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
@@ -7,17 +8,44 @@ import { MayusculasPipe } from '../../pipes/mayusculas.pipe';
   styles: [
   ]
 })
-export class OrdenarComponent implements OnInit {
+export class OrdenarComponent {
 
-  enMayusculas: boolean = true; 
+  enMayusculas: boolean = true;
+  heroes: Heroe[] = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.verde
+    },
+    {
+      nombre: 'DareDevil',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Linterna Verde',
+      vuela: true,
+      color: Color.verde
+    }
+  ];
 
-  CambiarMayusculas(){ 
+
+
+  CambiarMayusculas() {
     this.enMayusculas = !this.enMayusculas;
   }
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+
 
 }
